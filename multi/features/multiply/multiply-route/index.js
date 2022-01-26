071a776a7398e672f5e7eed3fddd2efcc57e1a6b
Fernando Amaro Vivoras <fernan.amaro@gmail.com>
2022-01-26 18:40:59 +0000

    last changes

--- multi/features/multiply/multiply-route/index.js
+++ multi/features/multiply/multiply-route/index.js
@@ -1,6 +1,17 @@
 const multiplyRoute = (req, reply) => {
     console.log(req.body);
 
+    if (req.body.input.num1 === 5){
+        reply.status(418).send({
+            message: 'we do not like num 5',
+            extension: {
+                path: "$.input.num1",
+                code:666
+            }
+        })
+    }
+
+
     const result = req.body.input.num1 * req.body.input.num2;
     reply.send({result});
 }
