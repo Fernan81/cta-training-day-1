72a88697e0db83e241d53a0cafcd41e82072b63c
Fernando Amaro Vivoras <fernan.amaro@gmail.com>
2022-02-02 09:00:27 +0000

    Add APP React

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import './index.css';
 import App from './App';
-
-const client = new ApolloClient({
-  uri: 'https://8080-fernan81-ctatrainingday1-6miupx7i8ib.ws-eu29.gitpod.io/v1/graphql',
-  cache: new InMemoryCache(),
-});
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ app_old/src/App.js
@@ -0,0 +1,12 @@
+import UserInsert from './AddUser';
+
+function App() {
+  
+  return (
+    <div>
+      <UserInsert />
+    </div>
+  );
+}
+
+export default App;
